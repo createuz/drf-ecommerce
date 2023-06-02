be4812ec62f75dda458ba53e0f9b2ca877dee694
createuz <seniordevv@gmail.com>
2023-06-02 20:18:41 +0500

    some additional functions have been added

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # DRFShop
 Online shop created with Django Rest API
+
+### Features
+ - Login & Registration 
+ - Ability to create shops in a short time
+ - Configure your shops very easy
+ - Free sub domain and SSL Certificates
+ - Separated swagger panel for main and specific shops
+ - Fully tested with pytest
+ - and more...
+ 
+ 
+### Used
+- Celery
+- Redis
+- Django Rest Framework
+- Swagger
+- Flake8
+
+## Screenshots
+<details> <summary> <h3>click to open here 👈</h3> </summary>
+
+### API for specific shops this url shows when you enter this url *subdomain.domain.com*
+![](https://telegra.ph/file/1f36bbbc29eb01f89200f.png)
+
+### Main API urls for configuring and creating shops and account
+![](https://telegra.ph/file/a6319db8607b3faf9f111.png)
+
+
+## custom admin panel
+![enter image description here](https://telegra.ph/file/10b194d8ac5b116182546.png)
+ </details>
--- api/urls.py
+++ api/urls.py
@@ -75,10 +75,23 @@ from .views import (
 )
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
-router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
+
+    # Products
+    path('products/', ProductViewSet.as_view({'get': 'list', 'post': 'create'}), name='product-list'),
+    path('products/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+         name='product-detail'),
+
+    # Categories
+    path('categories/', CategoryViewSet.as_view({'get': 'list', 'post': 'create'}), name='category-list'),
+    path('categories/<int:pk>/', CategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+         name='category-detail'),
+
+    # Send Email
+    path('send-email', SendMail.as_view(), name='send_email'),
     path('send-email/', SendMail.as_view(), name='send_email'),
     path('add-to-card', AddToShoppingCardAPIView.as_view(), name='shopping_card'),
     path('user-card', UserShoppingCardAPIView.as_view(), name='user_card'),
@@ -88,3 +101,14 @@ urlpatterns = [
     path('user-like-delete/<int:pk>', DeleteFromLikeAPIView.as_view(), name='user_like_delete'),
     path('', include(router.urls)),
 ]
+'''
+    GET /products/: Get a list of products.
+    POST /products/: Add new product.
+    PUT /products/<pk>/: Update a specific product.
+    DELETE /products/<pk>/: Delete a specific product.
+    GET /products/<pk>/: Get details of a particular product.
+    GET /categories/: Get a list of categories.
+    POST /categories/: Add new category.
+    PUT /categories/<pk>/: Update a specific category.
+    DELETE /categories/<pk>/: Delete a specific category.
+'''
